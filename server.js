17c6a1dd081aa0b0199001a02bc5f9df50ba013f
Shantanu Gupta <guptashantanu43@gmail.com>
2023-12-17 16:52:28 +0000

    added 'concurrently' to run both node , react

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const pool = require('./src/components/db');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
