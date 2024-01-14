b23ea315520307c9208aa3e0fa9495885006afc6
Shantanu Gupta <guptashantanu43@gmail.com>
2024-01-14 23:46:22 +0000

    express script to get all index.html

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(express.json());
 // Salt rounds for bcrypt hashing
 const saltRounds = 10;
 
+app.get("/*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+})
+
 app.get('/api/health', async (req, res) => {
   try {
     // Check the connection status
