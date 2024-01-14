e60c0c892f9d8a486d307bdedd919f5567814f36
Shantanu Gupta <guptashantanu43@gmail.com>
2024-01-14 23:16:31 +0000

    revert express script for api web deploy

--- server.js
+++ server.js
@@ -15,15 +15,6 @@ app.use(express.json());
 // Salt rounds for bcrypt hashing
 const saltRounds = 10;
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
-
 app.get('/api/health', async (req, res) => {
   try {
     // Check the connection status
